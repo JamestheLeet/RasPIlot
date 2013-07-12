625b657d6984934945887b9627dac14294b95d56
Jane <jane@localhost.localdomain>
2013-07-12 03:08:33 -0500

    Updated

--- gpsutils.py
+++ gpsutils.py
@@ -4,15 +4,16 @@ import gps
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
  
-while True:
-  report = session.next()
-  if report['class'] == 'TPV':
-    if hasattr(report, 'lat'):
-      lat = report.lat
-    if hasattr(report, 'lon'):
-      lon = report.lon
-    if hasattr(report, 'track'):
-      heading = report.track
+def gpsread():
+  while True:
+    report = session.next()
+    if report['class'] == 'TPV':
+      if hasattr(report, 'lat'):
+	lat = report.lat
+      if hasattr(report, 'lon'):
+	lon = report.lon
+      if hasattr(report, 'track'):
+	heading = report.track
       
 def getloc():
   loc = (lat, lon)
--- pilot.py
+++ pilot.py
@@ -1,4 +1,9 @@
-from gpsutils import *
+import gpsutils
+from multiprocessing import Process
 
-loc = gpsutils.getloc()
-print loc
+if __name__ == '__main__':
+    p = Process(target=gpsread, args=())
+    p.start()
+    p.join()
+    location = gpsutils.getloc()
+    head = gpsutils.gethead()
